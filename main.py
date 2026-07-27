4a5459c09b807e7a49789def8dfc833e5aa5c7f9
umut1241 <umutdenizb12@gmail.com>
2026-07-27 15:29:56 +0300

    .

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def sign_up():
         verification = input("Enter An Animal Name To Later Verification: ")
 
         with open("user_info.txt", "a") as f:
-            f.write("%-15s|%-15s|%-15s\n"%(username,password,verification))
+            f.write("%-15s| %-15s| %-15s\n"%(username,password,verification))
     else:
         with open("user_info.txt", "w", encoding="utf-8") as f:
-            f.write("%-15s|%-15s|%-15s\n"%("USERNAME", "PASSWORD", "VERIFICATION"))
+            f.write("%-15s| %-15s| %-15s\n"%("USERNAME", "PASSWORD", "VERIFICATION"))
 
 def sign_in():
     username = input("Enter Username: ")

0fd0b7d3d1e613092baadbc26e8beac39ef95db4
vryntrk <184443694+vryntrk@users.noreply.github.com>
2026-07-27 15:19:06 +0300

    signUp func update

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import os
+
 def sign_up():
-    username = input("Enter Username: ")
-    password = input("Enter Password: ")
-    verification = input("Enter An Animal Name To Later Verification: ")
+    if os.path.exists("user_info.txt"):
+        username = input("Enter Username: ")
+        password = input("Enter Password: ")
+        verification = input("Enter An Animal Name To Later Verification: ")
+
+        with open("user_info.txt", "a") as f:
+            f.write(f"{username} | {password} | {verification}")
+    else:
+        with open("user_info.txt", "w", encoding="utf-8") as f:
+            f.write("USERNAME | PASSWORD | VERIFICATION\n")
 
 def sign_in():
     username = input("Enter Username: ")

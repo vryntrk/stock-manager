eef6b52a6b8f79cdac1e14482ece6f3b06b16608
umut1241 <umutdenizb12@gmail.com>
2026-07-27 17:24:44 +0300

    edit function added

--- main.py
+++ main.py
@@ -31,4 +31,72 @@ def sign_in():
         sign_up()
 
 
-sign_up()
+def edit_user(username, password):
+    df_user = pd.read_csv("user_info.txt", sep=",", engine="python", header=None,
+                          names=["Username", "Password", "Verification"])
+    usernames = list(df_user["Username"])
+    passwords = list(df_user["Password"])
+
+
+
+    while True:
+
+        print("Editing Options: "
+              "1) Only Username"
+              "2) Only Password"
+              "3) Both"
+              "4) Exit"
+              "(Enter 1-4)")
+
+        option = int(input())
+
+        if option == 1:
+            while True:
+                new_username = input("Enter New Username: ")
+
+                if new_username in usernames:
+                    print("Username Already Exists!")
+                    continue
+
+                df_user.loc[usernames.index(username),"Username"] = new_username
+                print("Username Successfully Changed.")
+                break
+
+
+        if option == 2:
+            while True:
+                new_password = input("Enter New Password: ")
+
+                if new_password in passwords:
+                    print("Password Already Exists!")
+                    continue
+
+                df_user.loc[passwords.index(password),"Password"] = new_password
+                print("Password Successfully Changed.")
+                break
+
+        if option == 3:
+            while True:
+                new_username = input("Enter New Username: ")
+
+                if username in usernames:
+                    print("Username Already Exists!")
+                    continue
+
+                new_password = input("Enter New Password: ")
+
+                if password in passwords:
+                    print("Password Already Exists!")
+                    continue
+
+                df_user.loc[passwords.index(password)] = [new_username, new_password]
+
+                print("Username And Password Successfully Changed.")
+                break
+
+        if option == 4:
+            break
+
+
+
+

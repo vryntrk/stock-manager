ee96dfc030be925034ddf20bb00eba88858e779e
vryntrk <184443694+vryntrk@users.noreply.github.com>
2026-07-27 14:49:27 +0300

    signUp func

--- main.py
+++ main.py
@@ -1,1 +1,4 @@
-"""STOCK MANAGING APPLICATION"""
+def sign_up():
+    username = input("Enter Username: ")
+    password = input("Enter Password: ")
+    verification = input("Enter An Animal Name: ")

24d127f529c5e3d568c14823ab66f592cd693001
umut1241 <umutdenizb12@gmail.com>
2026-07-27 15:10:33 +0300

    sign_in

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 def sign_up():
     username = input("Enter Username: ")
     password = input("Enter Password: ")
-    verification = input("Enter An Animal Name: ")
+    verification = input("Enter An Animal Name To Later Verification: ")
+
+def sign_in():
+    username = input("Enter Username: ")
+    password = input("Enter Password: ")

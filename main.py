cb8f01a725efd5d7ff14ea8cbefa71d924b2b1cb
vryntrk <184443694+vryntrk@users.noreply.github.com>
2026-07-27 15:37:52 +0300

    funcs are updated

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ def sign_up():
         verification = input("Enter An Animal Name To Later Verification: ")
 
         with open("user_info.txt", "a") as f:
-            f.write("%-15s| %-15s| %-15s\n"%(username,password,verification))
+            f.write("%-15s| %-15s| %-15s\n"%(username, password, verification))
+            print("User Registered Successfully")
     else:
         with open("user_info.txt", "w", encoding="utf-8") as f:
             f.write("%-15s| %-15s| %-15s\n"%("USERNAME", "PASSWORD", "VERIFICATION"))
+            print("Database File Created")
 
 def sign_in():
-    username = input("Enter Username: ")
-    password = input("Enter Password: ")
+    if os.path.exists("user_info.txt"):
+        username = input("Enter Username: ")
+        password = input("Enter Password: ")
+    else:
+        sign_up()

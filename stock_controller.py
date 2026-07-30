01736c8e6adc98ea65cc795cb260ca57c8110d92
vryntrk <184443694+vryntrk@users.noreply.github.com>
2026-07-30 18:02:10 +0300

    add func updated

--- stock_controller.py
+++ stock_controller.py
@@ -1,12 +1,55 @@
 def add(username):
-    product_category = input("Enter Product Category: ")
-    product_name = input("Enter Product Name: ")
-    product_amount = float(input("Enter Product Amount: "))
-    product_unit = input("Enter Product Unit: ")
-    product_cost = float(input("Enter Product Cost: "))
-    product_price = float(input("Enter Product Price: "))
+    while True:
+        product_category = input("Enter Product Category: ")
+        try:
+            if not product_category.replace(" ", "").isalpha():
+                raise TypeError
+            break
+        except TypeError:
+            print("Product Category Must Contain Only Letters")
 
-    f = open(username + "_database.txt", "a", encoding="utf-8")
-    f.write(f"{product_category.title()},{product_name.title()},{product_amount},{product_unit.upper()},{product_cost},{product_price}\n")
-    f.close()
-    print("Product Successfully Added.")
+    while True:
+        product_name = input("Enter Product Name: ")
+        try:
+            if not product_name.replace(" ", "").isalpha():
+                raise TypeError
+            break
+        except TypeError:
+            print("Product Name Must Contain Only Letters")
+
+    while True:
+        product_amount = input("Enter Product Amount: ")
+        try:
+            float(product_amount)
+            break
+        except ValueError:
+            print("Product Amount Must Contain Only Numbers")
+
+    while True:
+        product_unit = input("Enter Product Unit: ")
+        try:
+            if not product_unit.isalpha():
+                raise TypeError
+            break
+        except TypeError:
+            print("Product Unit Must Contain Only Letters")
+
+    while True:
+        product_cost = input("Enter Product Cost: ")
+        try:
+            float(product_cost)
+            break
+        except ValueError:
+            print("Product Cost Must Contain Only Numbers")
+
+    while True:
+        product_price = input("Enter Product Price: ")
+        try:
+            float(product_price)
+            f = open(username + "_database.txt", "a", encoding="utf-8")
+            f.write(f"{product_category.title()},{product_name.title()},{product_amount},{product_unit.upper()},{product_cost},{product_price}\n")
+            f.close()
+            print("Product Successfully Added.")
+            break
+        except ValueError:
+            print("Product Price Must Contain Only Numbers")
